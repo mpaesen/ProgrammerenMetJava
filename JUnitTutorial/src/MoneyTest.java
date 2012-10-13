b8b9edab289564d10c79761c0413f082836ad66e
mpaesen <mpaesen@gmail.com>
2012-10-13 15:31:06 +0200

    Initial load JUnitTutorial

---
+++ JUnitTutorial/src/MoneyTest.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MoneyTest {
+	private Money money;
+	private Money moneyCopy;
+	@Before
+	public void setUp() throws Exception {
+		money = new Money(500, "€");
+		moneyCopy = new Money(money);
+	}
+
+	@Test
+	public void testAmount() {
+		//fail("Not yet implemented");
+		assertTrue(money.getfAmount() == 500);
+	}
+
+	@Test
+	public void testCurrency() {
+		//fail("Not yet implemented");
+		assertTrue(money.getfCurrency().equals("€"));
+	}
+
+	@Test
+	public void testEqualsObject() {
+		//fail("Not yet implemented");
+		assertTrue(money.equals(moneyCopy));
+	}
+
+	@Test
+	public void testAdd() {
+		//fail("Not yet implemented");
+		Money addition = money.add(moneyCopy);	
+		assertTrue(addition.getfAmount() == 1000);
+	}
+
+}
