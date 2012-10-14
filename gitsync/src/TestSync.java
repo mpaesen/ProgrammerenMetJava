88ad90d7a6640238cce6d6c37ab76984fbc38dc3
mpaesen <mpaesen@gmail.com>
2012-10-14 22:02:44 +0200

    20121014 upload modif

--- gitsync/src/TestSync.java
+++ gitsync/src/TestSync.java
@@ -1,13 +1,13 @@
-
 public class TestSync
 {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args)
+	public static void main(final String[] args)
 	{
 		// TODO Auto-generated method stub
+		System.out.printf("%s", "Original");
 
 	}
 
