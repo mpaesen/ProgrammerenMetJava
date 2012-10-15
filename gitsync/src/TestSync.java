dbdb1a47cc7cdaf8adff44da2a6ab131764093ec
mpaesen <mpaesen@gmail.com>
2012-10-15 15:07:55 +0200

    20121015 update VM

--- gitsync/src/TestSync.java
+++ gitsync/src/TestSync.java
@@ -8,7 +8,7 @@ public class TestSync
 	{
 		// TODO Auto-generated method stub
 		System.out.printf("%s", "Original");
-		System.out.printf("%s", "Copy");
+		
 		System.out.printf("%s", "Win7");
 		System.out.printf("%s", "Juno");
 	}
