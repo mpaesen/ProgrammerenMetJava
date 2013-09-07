2dbc2d2af970fd17e1993fa58b06077f8982bbb4
mpaesen <mpaesen@gmail.com>
2013-09-07 17:03:05 +0200

    Bug verwijderd

--- test/src/be/groept/HelloWorld.java
+++ test/src/be/groept/HelloWorld.java
@@ -1,10 +1,10 @@
 package be.groept;
 
 public class HelloWorld {
-
+	public static final int ZERO = 0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		if(args != null)
+		if(args.length == ZERO)
 			System.out.printf("%s", args[0]);
 		else
 			System.out.printf("%s", "Hallo allemaal");
