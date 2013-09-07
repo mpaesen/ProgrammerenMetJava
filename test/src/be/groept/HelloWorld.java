1a03fb97c61242976ae68fd8f07ec3f27aa719a0
mpaesen <mpaesen@gmail.com>
2013-09-07 16:59:20 +0200

    Hello gewijzigd

--- test/src/be/groept/HelloWorld.java
+++ test/src/be/groept/HelloWorld.java
@@ -4,7 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.printf("%s", args[0]);
+		if(args != null)
+			System.out.printf("%s", args[0]);
+		else
+			System.out.printf("%s", "Hallo allemaal");
 	}
 
 }
