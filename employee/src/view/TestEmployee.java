ac84c423d1f152eea7fa7a4078eb6dd6f953ff7c
mpaesen <mpaesen@gmail.com>
2013-09-29 20:15:14 +0200

    UI refactoring

--- employee/src/view/TestEmployee.java
+++ employee/src/view/TestEmployee.java
@@ -6,67 +6,67 @@ import javax.swing.JOptionPane;
 
 import business.Employee;
 
-public class TestEmployee
-{
+public class TestEmployee {
 
 	/**
 	 * @param args
 	 */
-	public static void main(final String[] args)
-	{
+	public static void main(final String[] args) {
 		final ArrayList<Employee> list = new ArrayList<Employee>();
 		mainIteration(list);
 		showOutput(list);
 		showOneEmployee(list);
 	}
 
-	public static void showOneEmployee(final ArrayList<Employee> list)
-	{
+	public static void showOneEmployee(final ArrayList<Employee> list) {
+		String input;
+		int i;
+		String range = "1 - ";
+		range += list.size();
+		do {
+			do {
+				input = getInput("Give a employee Number ( "+range+"): ");
+			} while (input.isEmpty());
 
-		final String input = getInput("Give a employee Number: ");
-		final int i = Integer.parseInt(input);
+			i = Integer.parseInt(input);
+			i-=1;
+		} while ((i < 0) || (i > list.size()-1));
 		System.out.println(list.get(i));
 	}
 
 	/**
 	 * @param list
 	 */
-	private static void showOutput(final ArrayList<Employee> list)
-	{
-		for (final Employee employees : list)
-		{
+	private static void showOutput(final ArrayList<Employee> list) {
+		for (final Employee employees : list) {
 			System.out.println(employees);
 		}
-		System.out.println("We have created: " + Employee.getSequence() + " employees.");
+		System.out.println("We have created: " + Employee.getSequence()
+				+ " employees.");
 
 	}
 
 	/**
 	 * @param list
 	 */
-	private static void mainIteration(final ArrayList<Employee> list)
-	{
+	private static void mainIteration(final ArrayList<Employee> list) {
 		String input;
 		String first;
 		String name;
 		Employee employee;
-		do
-		{
+		do {
 			input = getInput("Give firstname (Q to quit, anything else to continue");
-			if (!input.equalsIgnoreCase("Q"))
-			{
+			if (!input.equalsIgnoreCase("Q")) {
 				first = input;
 				input = getInput("Give the name :");
 				name = input;
 				employee = new Employee(name, first);
 				list.add(employee);
 			}
-		}
-		while (!input.equalsIgnoreCase("Q"));
+		} while (!input.equalsIgnoreCase("Q"));
 	}
 
-	public static String getInput(final String message)
-	{
+	public static String getInput(final String message) {
 		return JOptionPane.showInputDialog(message);
 	}
 }
