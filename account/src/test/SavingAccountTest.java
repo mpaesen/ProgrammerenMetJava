48085f54232777230ed0edce43d96e724778cd11
mpaesen <mpaesen@gmail.com>
2013-11-24 22:51:57 +0100

    Small modifications

---
+++ account/src/test/SavingAccountTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SavingAccountTest {
+
+
+	@Test
+	public void testAddTransaction() {
+		//fail("Not yet implemented");
+		
+	}
+
+}
