3585f9f31f9fa3473967a6ea2d405a32b7883a4e
mpaesen <mpaesen@gmail.com>
2014-11-07 21:33:00 +0100

    Small modif

--- lotto/lotto/ui/LottoControleGUI.java
+++ lotto/lotto/ui/LottoControleGUI.java
@@ -22,8 +22,6 @@ import lotto.model.LottoCombination;
 /**
  * @author bempn
  *
- * To change the template for this generated type comment go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
 public class LottoControleGUI extends JFrame {
 	/**
