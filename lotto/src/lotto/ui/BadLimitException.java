ae121843f89b6a53a00669a145e9985490057f3d
Mathy Paesen <mpaesen@gmail.com>
2018-11-12 12:50:50 +0100

    Small update

--- lotto/src/lotto/ui/BadLimitException.java
+++ lotto/src/lotto/ui/BadLimitException.java
@@ -7,7 +7,7 @@
 package lotto.ui;
 
 /**
- * @author bempn
+ * @author Mathy
  * 
  * To change the template for this generated type comment go to
  * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
